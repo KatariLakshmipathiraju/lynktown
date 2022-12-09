262aafb4422cb206b20593c22a2337fd63d952a8
Yogesh Kumar <77193839+Yogesh011-R@users.noreply.github.com>
2022-12-09 19:04:14 +0530

    google analytics && gTag

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -7,25 +7,15 @@ import Head from 'next/head';
 import Script from 'next/script';
 
 const Layout = ({ children }) => {
-  // useEffect(()=>{
-  //   window.dataLayer = window.dataLayer || [];
-  //   function gtag(){dataLayer.push(arguments)};
-  //   gtag('js', new Date());
-  //   gtag('config', 'G-TTP5FCW4FD');
-  // },[])
   return (
     <>
-      <Script
-        id='googletagmanager-js'
-        src='https://www.googletagmanager.com/gtag/js?id=G-TTP5FCW4FD'
-      />
       <Head>
         <title>LynkTown</title>
 
-        <meta
+        {/* <meta
           name='google-site-verification'
           content='2yo2OS7PnxSDhBmhGnE54wC83Lsl4doztBNsbMaxpds'
-        />
+        /> */}
 
         <meta
           name='description'
