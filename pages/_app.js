54c1094901fe26469c62a09acaa1c7e6f82e1ca4
Yogesh Kumar <77193839+Yogesh011-R@users.noreply.github.com>
2022-12-20 14:29:29 +0530

    gtag issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { NextGtag } from 'next-gtag';
 
 function MyApp({ Component, pageProps }) {
-  return  <Component {...pageProps} />
+  return (
+    <>
+      <NextGtag trackingId='GTM-MF5LB4V' />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,34 +1,15 @@
+import { gtagScript } from 'next-gtag';
 import { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
 const Document = () => {
   return (
     <Html>
-      <Head />
-
-      <Script id='google-analytics' strategy='afterInteractive'>
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MF5LB4V');
-        `}
-      </Script>
-
+      <Head />{' '}
       <body>
         <Main />
         <NextScript />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `  <iframe
-            src='https://www.googletagmanager.com/ns.html?id=GTM-MF5LB4V'
-            height='0'
-            width='0'
-            style='display:none;visibility:hidden'
-          ></iframe>`,
-          }}
-        ></noscript>
+        {gtagScript}
       </body>
     </Html>
   );
