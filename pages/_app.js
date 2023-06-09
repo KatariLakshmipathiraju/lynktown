e53670c27671776639eaafef981cf31d7d829faa
arun kumar <arun.k270892@gmail.com>
2023-06-09 07:03:43 +0530

    fixed package.json

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
 import "../styles/globals.css";
-import { NextGtag } from "next-gtag";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <NextGtag trackingId="GTM-MF5LB4V" /> */}
-
       <Component {...pageProps} />
     </>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -1,51 +1,10 @@
-// import { gtagScript } from 'next-gtag';
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 const Document = () => {
   return (
     <Html>
-      <Head>
-        <Script
-          strategy="lazyOnload"
-          id="gtm-script"
-          dangerouslySetInnerHTML={{
-            __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MF5LB4V');
-      `,
-          }}
-        />
-        {/* <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MF5LB4V');`,
-          }}
-        ></Script> */}
-      </Head>
+      <Head></Head>
       <body>
-        {/* <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MF5LB4V"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript> */}
-        {/* <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MF5LB4V"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        ></noscript> */}
         <Main />
         <NextScript />
       </body>
