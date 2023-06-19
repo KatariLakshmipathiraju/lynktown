5559a3793dd7f9a53311d0b3ffa0ce49c25e1f3c
Lovekr <lovek468@gmail.com>
2023-06-19 22:42:03 +0530

    Added next config

--- next.config.js
+++ next.config.js
@@ -3,12 +3,16 @@ const nextConfig = {
   images: {
     domains: ["localhost", "api.lynktown.in"],
   },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'api.lynktown.in',
+      port: '',
+      pathname: '/**',
+    },
+],
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
 };
 
 module.exports = nextConfig;
