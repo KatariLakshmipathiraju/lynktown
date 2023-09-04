692e39b3efba289a10f0fe14b9ebb0933d695309
Arun k <arun923016@gmail.com>
2023-09-04 17:07:47 +0530

    added google analytics

--- components/HomePage/ExploreDesigners.jsx
+++ components/HomePage/ExploreDesigners.jsx
@@ -2,6 +2,34 @@ import Link from "next/link";
 import Image from "next/image";
 import { CiLocationOn } from "react-icons/ci";
 
+const Designers = [
+  {
+    designerName: "Jo’s Fashion",
+    image: "/assets/exploreDesigners.svg",
+    location: "Kalyan nagar, Bangalore",
+  },
+  {
+    designerName: "Fulki Designer Studio",
+    image: "/assets/exploreDesigners.svg",
+    location: "Mumbai, Maharashtra",
+  },
+  {
+    designerName: "Bratt Heww",
+    image: "/assets/exploreDesigners.svg",
+    location: "Chamrajpeth, Bangalore",
+  },
+  {
+    designerName: "Aarat by Aakshi",
+    image: "/assets/exploreDesigners.svg",
+    location: "Jaipur, Rajasthan",
+  },
+  {
+    designerName: "Fulki Designer Studio",
+    image: "/assets/exploreDesigners.svg",
+    location: "Mumbai, Maharashtra",
+  },
+];
+
 export default function ExploreDesigners() {
   return (
     <main className="px-5 sm:px-8 md:px-10 xl:px-16 mt-14 xl:mt-20 flex flex-col gap-4">
@@ -10,153 +38,36 @@ export default function ExploreDesigners() {
       </h1>
 
       <section className="mb-4 mt-10 overflow-x-auto">
-        <div className="flex gap-5 ">
-          {/* first card  */}
-          <div className="rounded-2xl flex flex-col gap-1 bg-[#FFF4E2] border border-[#6D5C41] min-w-[250px] h-64 text-center px-6 py-4">
-            <Image
-              src="/assets/exploreDesigners.svg"
-              alt="explore designers"
-              width={118}
-              height={118}
-              className="rounded-full border-2 border-[#6D5C41] object-contain mx-auto"
-            />
-            <h1>Jo’s Fashion</h1>
-            <p className="flex gap-1 justify-center items-center opacity-80 whitespace-nowrap">
-              <span className=" inline-block">
-                <CiLocationOn size={18} />
-              </span>
-              <span className="text-sm"> Kalyan nagar, Bangalore</span>
-            </p>
-            <Link
-              href="https://app.lynktown.com/designers"
-              className="text-[#6D5C41] underline font-semibold mt-2"
+        <div className="flex gap-5">
+          {Designers.map((designer, index) => (
+            <div
+              key={index}
+              className="rounded-2xl flex flex-col gap-1 bg-[#FFF4E2] border border-[#6D5C41] min-w-[250px] h-64 text-center px-6 py-4"
             >
-              Explore Lynks
-            </Link>
-          </div>
-
-          {/* second card  */}
-          <div className="rounded-2xl flex flex-col gap-1 bg-[#FFF4E2] border border-[#6D5C41] min-w-[250px] h-64 text-center px-6 py-4">
-            <Image
-              src="/assets/exploreDesigners.svg"
-              alt="explore designers"
-              width={118}
-              height={118}
-              className="rounded-full border-2 border-[#6D5C41] object-contain mx-auto"
-            />
-            <h1>Fulki Designer Studio</h1>
-            <p className="flex gap-1 justify-center items-center opacity-80 whitespace-nowrap text-sm">
-              <span className=" inline-block">
-                <CiLocationOn size={18} />
-              </span>
-              <span> Mumbai, Maharashtra</span>
-            </p>
-            <Link
-              href="https://app.lynktown.com/designers"
-              className="text-[#6D5C41] underline font-semibold mt-2"
-            >
-              Explore Lynks
-            </Link>
-          </div>
-
-          {/* third card  */}
-          <div className="rounded-2xl flex flex-col gap-1 bg-[#FFF4E2] border border-[#6D5C41] min-w-[250px] h-64 text-center px-6 py-4">
-            <Image
-              src="/assets/exploreDesigners.svg"
-              alt="explore designers"
-              width={118}
-              height={118}
-              className="rounded-full border-2 border-[#6D5C41] object-contain mx-auto"
-            />
-            <h1>Bratt Heww</h1>
-            <p className="flex gap-1 justify-center items-center opacity-80 whitespace-nowrap text-sm">
-              <span className=" inline-block">
-                <CiLocationOn size={18} />
-              </span>
-              <span> Chamrajpeth, Bangalore</span>
-            </p>
-            <Link
-              href="https://app.lynktown.com/designers"
-              className="text-[#6D5C41] underline font-semibold mt-2"
-            >
-              Explore Lynks
-            </Link>
-          </div>
-
-          {/* fourth card  */}
-          <div className="rounded-2xl flex flex-col gap-1 bg-[#FFF4E2] border border-[#6D5C41] min-w-[250px] h-64 text-center px-6 py-4">
-            <Image
-              src="/assets/exploreDesigners.svg"
-              alt="explore designers"
-              width={118}
-              height={118}
-              className="rounded-full border-2 border-[#6D5C41] object-contain mx-auto"
-            />
-            <h1>Aarat by Aakshi</h1>
-            <p className="flex gap-1 justify-center items-center opacity-80 whitespace-nowrap text-sm">
-              <span className=" inline-block">
-                <CiLocationOn size={18} />
-              </span>
-              <span> Jaipur, Rajasthan</span>
-            </p>
-            <Link
-              href="https://app.lynktown.com/designers"
-              className="text-[#6D5C41] underline font-semibold mt-2"
-            >
-              Explore Lynks
-            </Link>
-          </div>
-
-          {/* fifth card  */}
-          <div className="rounded-2xl flex flex-col gap-1 bg-[#FFF4E2] border border-[#6D5C41] min-w-[250px] h-64 text-center px-6 py-4">
-            <Image
-              src="/assets/exploreDesigners.svg"
-              alt="explore designers"
-              width={118}
-              height={118}
-              className="rounded-full border-2 border-[#6D5C41] object-contain mx-auto"
-            />
-            <h1>Jo’s Fashion</h1>
-            <p className="flex gap-1 justify-center items-center opacity-80 whitespace-nowrap text-sm">
-              <span className=" inline-block">
-                <CiLocationOn size={18} />
-              </span>
-              <span> Kalyan nagar, Bangalore</span>
-            </p>
-            <Link
-              href="https://app.lynktown.com/designers"
-              className="text-[#6D5C41] underline font-semibold mt-2"
-            >
-              Explore Lynks
-            </Link>
-          </div>
-
-          {/* sixth card  */}
-          <div className="rounded-2xl flex flex-col gap-1 bg-[#FFF4E2] border border-[#6D5C41] min-w-[250px] h-64 text-center px-6 py-4">
-            <Image
-              src="/assets/exploreDesigners.svg"
-              alt="explore designers"
-              width={118}
-              height={118}
-              className="rounded-full border-2 border-[#6D5C41] object-contain mx-auto"
-            />
-            <h1>Aarat by Aakshi</h1>
-            <p className="flex gap-1 justify-center items-center opacity-80 whitespace-nowrap text-sm">
-              <span className=" inline-block">
-                <CiLocationOn size={18} />
-              </span>
-              <span> Jaipur, Rajasthan </span>
-            </p>
-            <Link
-              href="https://app.lynktown.com/designers"
-              className="text-[#6D5C41] underline font-semibold mt-2"
-            >
-              Explore Lynks
-            </Link>
-          </div>
+              <Image
+                src={designer.image}
+                alt={`Image of ${designer.designerName}`} 
+                width={118}
+                height={118}
+                className="rounded-full border-2 border-[#6D5C41] object-contain mx-auto"
+              />
+              <h1>{designer.designerName}</h1>
+              <p className="flex gap-1 justify-center items-center opacity-80 whitespace-nowrap">
+                <span className="inline-block">
+                  <CiLocationOn size={18} />
+                </span>
+                <span className="text-sm">{designer.location}</span>
+              </p>
+              <Link
+                href="https://app.lynktown.com/designers"
+                className="text-[#6D5C41] underline font-semibold mt-2"
+              >
+                Explore Lynks
+              </Link>
+            </div>
+          ))}
         </div>
       </section>
-
       <Link
         href="https://app.lynktown.com/designers"
         target="_blank"
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-0GKWK82P48`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-0GKWK82P48');
+    `}
+      </Script>
       <div className="max-w-[1440px] mx-auto">
         <Component {...pageProps} />
       </div>
