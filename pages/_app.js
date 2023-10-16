a6ff189b0bce88f7478eca428a03e3742f5c2ea5
Arun kumar <arun923016@gmail.com>
2023-10-16 12:59:06 +0530

    update GTM

--- pages/_app.js
+++ pages/_app.js
@@ -4,18 +4,6 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-0GKWK82P48`}
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-0GKWK82P48');
-    `}
-      </Script>
       <div className="max-w-[1440px] mx-auto">
         <Component {...pageProps} />
       </div>
--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,33 @@ const Document = () => {
   return (
     <Html>
       <Head>
-        <meta
+        <Script
+          strategy="afterInteractive"
+          id="gtm-script"
+          dangerouslySetInnerHTML={{
+            __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MF5LB4V');
+      `,
+          }}
+        />
+        {/* <meta
           name="google-site-verification"
           content="2yo2OS7PnxSDhBmhGnE54wC83Lsl4doztBNsbMaxpds"
-        />
+        /> */}
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MF5LB4V"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
